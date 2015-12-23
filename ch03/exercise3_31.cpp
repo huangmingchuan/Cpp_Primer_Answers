eca0bed1ac4f914477ea0fba13223bb95d53de1a
huangmingchuan <hmcxxdxx@gmail.com>
2015-12-23 16:50:01 +0800

    ch03 ex3.33

---
+++ ch03/exercise3_31.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+int main()
+{
+	int arr[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	return 0;
+}
---
+++ ch03/exercise3_32.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using std::cout; using std::endl; using std::vector;
+
+int main()
+{
+	// array
+	int arr[10];
+	for (int i = 0; i < 10; ++i) arr[i] = i;
+	int arr2[10];
+	for (int i = 0; i < 10; ++i) arr2[i] = arr[i];
+
+	// vector
+	vector<int> v(10);
+	for (int i = 0; i != 10; ++i) v[i] = arr[i];
+	vector<int> v2(v);
+	for (auto i : v2) cout << i << " ";
+	cout << endl;
+
+	return 0;
+}
