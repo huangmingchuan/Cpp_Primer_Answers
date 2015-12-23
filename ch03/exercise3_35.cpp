0e8071b5f312265d7199b333e6fca5d395718056
huangmingchuan <hmcxxdxx@gmail.com>
2015-12-23 17:14:15 +0800

    ch03 ex3.36

---
+++ ch03/exercise3_35.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout; 
+using std::endl;
+
+int main()
+{
+	const int size = 10;
+	int arr[size];
+	for (auto ptr = arr; ptr != arr + size; ++ptr) *ptr = 0;
+
+	for (auto i : arr) cout << i << " ";
+	cout << endl;
+
+	return 0;
+}
