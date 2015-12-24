b5d9562826b23f5a55279fd2c35264194307a30d
huangmingchuan <hmcxxdxx@gmail.com>
2015-12-24 16:26:16 +0800

    ch04 ex4.27

---
+++ ch04/exercise4_21.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main()
+{
+	vector<int> ivec{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	for (auto i : ivec)
+	{
+		cout << ((i & 0x1) ? i * 2 : i) << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
---
+++ ch04/exercise4_22.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout; using std::cin; using std::endl;
+
+int main()
+{
+	for (unsigned g; cin >> g;)
+	{
+		auto result = g > 90 ? "high pass" : g < 60 ? "fail" : g < 75 ? "low pass" : "pass";
+		cout << result << endl;
+
+		// -------------------------
+		if (g > 90)         cout << "high pass";
+		else if (g < 60)    cout << "fail";
+		else if (g < 75)    cout << "low pass";
+		else                cout << "pass";
+		cout << endl;
+	}
+
+	return 0;
+}
