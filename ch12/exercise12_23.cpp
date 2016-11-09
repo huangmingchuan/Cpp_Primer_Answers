4e008b125db08ca03def56888eb752b013ddc8d0
sanerror <237606586@qq.com>
2016-11-09 16:36:47 +0800

    Update exercise12_23.cpp

--- ch12/exercise12_23.cpp
+++ ch12/exercise12_23.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <string>
+#include <cstring>
+#include <memory>
 
-using namespace std;
+int main() {
+	const char *c1 = "Hello ";
+	const char *c2 = "World";
+	unsigned len = strlen(c1) + strlen(c2) + 1;
+	char *r = new char[len]();
+	strcat_s(r, len, c1);
+	strcat_s(r, len, c2);
+	std::cout << r << std::endl;
 
-int main()
-{
-	char *chs = new char[strlen("hello " "world") + 1]();
-	strcat(chs, "hello ");
-	strcat(chs, "world");
-	cout << chs << endl;
-	delete[] chs;
+	std::string s1 = "Hello ";
+	std::string s2 = "World";
+	strcpy_s(r, len, (s1 + s2).c_str());
+	std::cout << r << std::endl;
 
-	string s1("hello "), s2("world");
-	cout << s1 + s2 << endl;
+	delete[] r;
+
 	return 0;
 }
