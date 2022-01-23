689e9add237590614a56afc07570e59c9c6993a4
JPLAY <513963138@qq.com>
2022-01-23 19:13:15 +0800

    fix ex10_31

--- ch10/exercise10_31.cpp
+++ ch10/exercise10_31.cpp
@@ -10,10 +10,10 @@ int main()
 	vector<int> v;
 	istream_iterator<int> int_it(cin), int_eof;
 
-	unique_copy(int_it, int_eof, back_inserter(v));
+	copy(int_it, int_eof, back_inserter(v));
 	sort(v.begin(), v.end());
 
-	copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+	unique_copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
 	cout << endl;
 	return 0;
 }
